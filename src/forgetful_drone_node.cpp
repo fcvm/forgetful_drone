43d5da031dd4c0ba2884b9598c1e52ca82ab5de0
fcvm <fmang@outlook.de>
2022-06-02 17:58:07 +0200

    simplify forgetful_drone_node.cpp

--- src/forgetful_drone_node.cpp
+++ src/forgetful_drone_node.cpp
@@ -1,38 +1,10 @@
-#include <gflags/gflags.h>
-#include <glog/logging.h>
 #include "forgetful_drones/forgetful_drone.hpp"
 
+int main (int argc, char** argv) {
+    ros::init (argc, argv, "forgetful_drone");
+    forgetful_drone::ForgetfulDrone fd;
+    ros::MultiThreadedSpinner s;
+    s.spin();
 
-
-
-
-int runForgetfulDrone( int argc, char** argv );
-
-int main( int argc, char** argv )
-{
-  //google::InitGoogleLogging( argv[0] );
-  //google::ParseCommandLineFlags( &argc, &argv, true );
-  //google::InstallFailureSignalHandler();
-  //FLAGS_alsologtostderr = true;
-  //FLAGS_colorlogtostderr = true;
-
-  return runForgetfulDrone(argc, argv);
-}
-
-int runForgetfulDrone(int argc, char** argv)
-{
-  if (false)
-  {
-    ros::Time::init();
-    forgetful_drone::generateRacetrackData();
-  }
-  else
-  {
-    ros::init( argc, argv, "forgetful_drone" );
-    forgetful_drone::ForgetfulDrone ForgetfulDrone;
-    ros::MultiThreadedSpinner Spinner;
-    Spinner.spin();
-  }
-  
-  return 0;
+    return 0;
 }
