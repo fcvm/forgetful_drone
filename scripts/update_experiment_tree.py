0d0bf01df38bf8e79d40768a2d3528d971a64cda
fcvm <fmang@outlook.de>
2022-08-10 13:46:49 +0200

    add all experiments to scripts

--- scripts/update_experiment_tree.py
+++ scripts/update_experiment_tree.py
@@ -1,29 +1,23 @@
-
-
 import json
 import rospkg; rospack = rospkg.RosPack()
 from pathlib import Path
 import filecmp
-
-
-from forgetful_brain import ForgetfulBrain
-fb = ForgetfulBrain ()
+from forgetful_brain import ForgetfulBrain; fb = ForgetfulBrain ()
 
 
 for EXP_ID in [
     #'UTC_2022_06_13_10_13_56',
-    'UTC_2022_07_01_07_27_56___SEQLEN_2',
-    'UTC_2022_07_01_07_27_56___SEQLEN_2___RF3',
-    'UTC_2022_07_01_07_27_56___SEQLEN_3___RF3',
-    'UTC_2022_07_01_07_27_56___SEQLEN_5___RF3', 
-    'UTC_2022_07_01_07_27_56___SEQLEN_10___RF3',
+    #'UTC_2022_06_22_21_15_38',
+    #'UTC_2022_07_01_07_27_56',
+    #'UTC_2022_07_01_07_27_56___SEQLEN_2',
+    #'UTC_2022_07_01_07_27_56___SEQLEN_5',
+    #'UTC_2022_07_01_07_27_56___SEQLEN_10',
+    #'UTC_2022_07_01_07_27_56___SEQLEN_25',
+    #'UTC_2022_07_01_07_27_56___SEQLEN_2___RF3',
+    #'UTC_2022_07_01_07_27_56___SEQLEN_3___RF3',
+    #'UTC_2022_07_01_07_27_56___SEQLEN_5___RF3', 
+    #'UTC_2022_07_01_07_27_56___SEQLEN_10___RF3',
 ]:
-
-    ################################################################
-    ################################################################
-    ################################################################
-    ################################################################
-    ################################################################
 
     def rmtree (p: Path) -> None:
         for s in p.iterdir (): s.unlink () if s.is_file () else rmtree (s)
